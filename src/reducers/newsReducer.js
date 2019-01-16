e780609166382159040b3229a9eb13471405fc82
sergeikd <sergeikd@gmail.com>
2019-01-16 11:27:48 +0300

    troubles with state

--- src/reducers/newsReducer.js
+++ src/reducers/newsReducer.js
@@ -3,21 +3,21 @@ const initialState = {
     isLoading: false,
 };
 
-export const isLoading = (state = false, action) => {
+export const isLoading = (state = initialState, action) => {
     switch (action.type) {
         case "NEWS_ARE_LOADING":
-            return action.isLoading;
-            // return {...state, isLoading: action.isLoading, news: []};
+            // return action.isLoading;
+            return {...state, isLoading: action.isLoading, news: []};
         default:
             return state;
     }
 }
 
-export const news = (state = [], action) => {
+export const news = (state = initialState, action) => {
     switch (action.type) {
         case "NEWS_FETCH_SUCCESS":
-            return action.news;
-            // return {...state, isLoading: false, news: action.news};
+            // return action.news;
+            return {...state, isLoading: false, news: action.news};
         default:
             return state;
     }
