841a33697fb4238c66258ea6f661eac3af15c5d5
sergeikd <sergeikd@gmail.com>
2019-01-18 18:09:58 +0300

    build

---
+++ precache-manifest.ce28a81eaa7f3597eb6d58ca1fa00e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c99ccc1f8e79cd72b1d3",
+    "url": "/news-react-redux-typescript/static/js/runtime~main.c99ccc1f.js"
+  },
+  {
+    "revision": "44bfa7c3179bb710e8a4",
+    "url": "/news-react-redux-typescript/static/js/main.44bfa7c3.chunk.js"
+  },
+  {
+    "revision": "bad298f1f5c55e7ce63c",
+    "url": "/news-react-redux-typescript/static/js/1.bad298f1.chunk.js"
+  },
+  {
+    "revision": "44bfa7c3179bb710e8a4",
+    "url": "/news-react-redux-typescript/static/css/main.c60be93c.chunk.css"
+  },
+  {
+    "revision": "da2bae4d45a1c3f9930b25c4dc1e3956",
+    "url": "/news-react-redux-typescript/index.html"
+  }
+];
